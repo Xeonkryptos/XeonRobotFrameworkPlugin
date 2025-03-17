8587be3c414d1de4ac94cf61cc585b34fbd8d3d3
Dennis Föller <dennis.foeller@adesso.de>
2025-03-17 13:50:36 +0100

    use custom runners independent from PythonRunner and PyDebugRunner

--- src/main/java/com/github/jnhyperion/hyperrobotframeworkplugin/ide/debugger/RobotDebugRunner.java
+++ src/main/java/com/github/jnhyperion/hyperrobotframeworkplugin/ide/debugger/RobotDebugRunner.java
@@ -7,10 +7,12 @@ import com.intellij.execution.ExecutionException;
 import com.intellij.execution.ExecutionResult;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.configurations.RunnerSettings;
 import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.execution.executors.DefaultRunExecutor;
 import com.intellij.execution.process.ProcessEvent;
 import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.runners.ProgramRunner;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.xdebugger.XDebugProcess;
@@ -27,9 +29,9 @@ import org.jetbrains.concurrency.Promise;
 
 import java.net.ServerSocket;
 
-public class RobotDebugRunner extends PyDebugRunner {
+public class RobotDebugRunner implements ProgramRunner<RunnerSettings> {
 
-    private ExecutionResult executionResult;
+    private final PyDebugRunner pyDebugRunner = new CustomPyDebugRunner();
 
     @NonNls
     @NotNull
@@ -40,48 +42,57 @@ public class RobotDebugRunner extends PyDebugRunner {
 
     @Override
     public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
-        if (super.canRun(executorId, profile) && profile instanceof RobotRunConfiguration) {
-            return DefaultDebugExecutor.EXECUTOR_ID.equals(executorId) || DefaultRunExecutor.EXECUTOR_ID.equals(executorId);
+        return profile instanceof RobotRunConfiguration && (DefaultDebugExecutor.EXECUTOR_ID.equals(executorId) || DefaultRunExecutor.EXECUTOR_ID.equals(
+                executorId));
+    }
+
+    @Override
+    public void execute(@NotNull ExecutionEnvironment environment) throws ExecutionException {
+        pyDebugRunner.execute(environment);
+    }
+
+    private static class CustomPyDebugRunner extends PyDebugRunner {
+
+        private ExecutionResult executionResult;
+
+        @NotNull
+        @Override
+        protected Promise<@Nullable RunContentDescriptor> execute(@NotNull ExecutionEnvironment environment, @NotNull RunProfileState state) throws
+                                                                                                                                             ExecutionException {
+            return super.execute(environment, state).then(result -> {
+                Project project = environment.getProject();
+                try {
+                    XDebuggerManager debuggerManager = XDebuggerManager.getInstance(project);
+                    Integer robotDebugPort = ((RobotPythonScriptCommandLineState) state).getRobotDebugPort();
+                    RobotDebugAdapterProtocolCommunicator robotDebugAdapterProtocolCommunicator = new RobotDebugAdapterProtocolCommunicator(robotDebugPort);
+                    RunContentDescriptor runContentDescriptor = debuggerManager.startSession(environment, new XDebugProcessStarter() {
+                        @NotNull
+                        @Override
+                        public XDebugProcess start(@NotNull final XDebugSession session) {
+                            return new RobotDebugProcess(session, executionResult, robotDebugAdapterProtocolCommunicator);
+                        }
+                    }).getRunContentDescriptor();
+
+                    executionResult.getProcessHandler().addProcessListener(robotDebugAdapterProtocolCommunicator);
+                    // Usually, startNotified would be called by the ProcessHandler itself and in reality, it is called by it. Sadly, when we're reaching this point,
+                    // the process is already running and the method called. Therefore, we have to emulate the call ourselves to connect to our debug server
+                    robotDebugAdapterProtocolCommunicator.startNotified(new ProcessEvent(executionResult.getProcessHandler()));
+
+                    return runContentDescriptor;
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         }
-        return false;
-    }
 
-    @Override
-    protected @NotNull Promise<@Nullable RunContentDescriptor> execute(@NotNull ExecutionEnvironment environment, @NotNull RunProfileState state) throws
-                                                                                                                                                  ExecutionException {
-        return super.execute(environment, state).then(result -> {
-            Project project = environment.getProject();
-            try {
-                XDebuggerManager debuggerManager = XDebuggerManager.getInstance(project);
-                Integer robotDebugPort = ((RobotPythonScriptCommandLineState) state).getRobotDebugPort();
-                RobotDebugAdapterProtocolCommunicator robotDebugAdapterProtocolCommunicator = new RobotDebugAdapterProtocolCommunicator(robotDebugPort);
-                RunContentDescriptor runContentDescriptor = debuggerManager.startSession(environment, new XDebugProcessStarter() {
-                    @NotNull
-                    @Override
-                    public XDebugProcess start(@NotNull final XDebugSession session) {
-                        return new RobotDebugProcess(session, executionResult, robotDebugAdapterProtocolCommunicator);
-                    }
-                }).getRunContentDescriptor();
-
-                executionResult.getProcessHandler().addProcessListener(robotDebugAdapterProtocolCommunicator);
-                // Usually, startNotified would be called by the ProcessHandler itself and in reality, it is called by it. Sadly, when we're reaching this point,
-                // the process is already running and the method called. Therefore, we have to emulate the call ourselves to connect to our debug server
-                robotDebugAdapterProtocolCommunicator.startNotified(new ProcessEvent(executionResult.getProcessHandler()));
-
-                return runContentDescriptor;
-            } catch (ExecutionException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    @NotNull
-    @Override
-    protected PyDebugProcess createDebugProcess(@NotNull XDebugSession session,
-                                                ServerSocket serverSocket,
-                                                ExecutionResult result,
-                                                PythonCommandLineState pyState) {
-        executionResult = result;
-        return super.createDebugProcess(session, serverSocket, result, pyState);
+        @NotNull
+        @Override
+        protected PyDebugProcess createDebugProcess(@NotNull XDebugSession session,
+                                                    ServerSocket serverSocket,
+                                                    ExecutionResult result,
+                                                    PythonCommandLineState pyState) {
+            executionResult = result;
+            return super.createDebugProcess(session, serverSocket, result, pyState);
+        }
     }
 }
--- src/main/java/com/github/jnhyperion/hyperrobotframeworkplugin/ide/execution/RobotRunner.java
+++ src/main/java/com/github/jnhyperion/hyperrobotframeworkplugin/ide/execution/RobotRunner.java
@@ -1,11 +1,18 @@
 package com.github.jnhyperion.hyperrobotframeworkplugin.ide.execution;
 
 import com.intellij.execution.configurations.RunProfile;
+import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.configurations.RunnerSettings;
+import com.intellij.execution.runners.AsyncProgramRunner;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.ui.RunContentDescriptor;
 import com.jetbrains.python.run.PythonRunner;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.concurrency.Promise;
 
-public class RobotRunner extends PythonRunner {
+public class RobotRunner extends AsyncProgramRunner<RunnerSettings> {
 
     @NonNls
     @NotNull
@@ -16,6 +23,20 @@ public class RobotRunner extends PythonRunner {
 
     @Override
     public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
-        return (super.canRun(executorId, profile) || RobotDryRunExecutor.EXECUTOR_ID.equals(executorId)) && profile instanceof RobotRunConfiguration;
+        return RobotDryRunExecutor.EXECUTOR_ID.equals(executorId) && profile instanceof RobotRunConfiguration;
+    }
+
+    @NotNull
+    @Override
+    protected Promise<RunContentDescriptor> execute(@NotNull ExecutionEnvironment executionEnvironment, @NotNull RunProfileState runProfileState) {
+        return new CustomPythonRunner().execute(executionEnvironment, runProfileState);
+    }
+
+    private static class CustomPythonRunner extends PythonRunner {
+
+        @Override
+        public @NotNull Promise<@Nullable RunContentDescriptor> execute(@NotNull ExecutionEnvironment env, @NotNull RunProfileState state) {
+            return super.execute(env, state);
+        }
     }
 }
