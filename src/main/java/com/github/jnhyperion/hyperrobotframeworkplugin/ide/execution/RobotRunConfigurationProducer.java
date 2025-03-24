82a84a279c7c45e91837127cb2da7552fdda7535
Dennis Föller <dennis.foeller@adesso.de>
2025-03-24 11:25:53 +0100

    don't overwrite working directory invalidly

--- src/main/java/com/github/jnhyperion/hyperrobotframeworkplugin/ide/execution/RobotRunConfigurationProducer.java
+++ src/main/java/com/github/jnhyperion/hyperrobotframeworkplugin/ide/execution/RobotRunConfigurationProducer.java
@@ -42,7 +42,7 @@ public class RobotRunConfigurationProducer extends LazyRunConfigurationProducer<
         if (isValidRobotExecutableScript(context)) {
             String workingDirectory = getWorkingDirectoryToUse(runConfig);
             String runParam = getRunParameters(context, workingDirectory);
-            runConfig.getPythonRunConfiguration().setWorkingDirectory(context.getProject().getBasePath());
+            runConfig.getPythonRunConfiguration().setWorkingDirectory(workingDirectory);
             runConfig.getPythonRunConfiguration().setScriptParameters(runParam);
             Sdk sdk = ProjectRootManager.getInstance(context.getProject()).getProjectSdk();
             if (sdk != null) {
