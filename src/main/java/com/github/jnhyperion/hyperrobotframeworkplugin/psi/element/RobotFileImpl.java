5042bc17fbbee676cd7d8064e1c075ea4e125507
Dennis Föller <dennis.foeller@adesso.de>
2025-05-06 11:32:12 +0200

    try to fix StackOverflowError

--- src/main/java/com/github/jnhyperion/hyperrobotframeworkplugin/psi/element/RobotFileImpl.java
+++ src/main/java/com/github/jnhyperion/hyperrobotframeworkplugin/psi/element/RobotFileImpl.java
@@ -207,7 +207,7 @@ public class RobotFileImpl extends PsiFileBase implements KeywordFile, RobotFile
 
     private void collectTransitiveKeywordFiles(Collection<KeywordFile> results, KeywordFile keywordFile, boolean includeTransitive) {
         if (results.add(keywordFile) && includeTransitive) {
-            for (KeywordFile child : keywordFile.getImportedFiles(true)) {
+            for (KeywordFile child : keywordFile.getImportedFiles(false)) {
                 collectTransitiveKeywordFiles(results, child, true);
             }
         }
@@ -218,7 +218,7 @@ public class RobotFileImpl extends PsiFileBase implements KeywordFile, RobotFile
                                                                      KeywordFile keywordFile,
                                                                      boolean includeTransitive) {
         if (results.add(new KeywordFileWithParentWrapper(keywordFile, parentFile)) && includeTransitive) {
-            for (KeywordFile child : keywordFile.getImportedFiles(true)) {
+            for (KeywordFile child : keywordFile.getImportedFiles(false)) {
                 collectTransitiveKeywordFilesWithDependencyTracking(results, keywordFile, child, true);
             }
         }
