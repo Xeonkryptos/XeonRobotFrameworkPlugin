744091c639352af05bb0ab11adb041f2c0300bf9
Dennis Föller <2-Xeonkryptos@users.noreply.gitlab.xeonkryptos.dev>
2025-05-24 07:05:11 +0200

    updated github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,6 @@ plugins {
     id("org.jetbrains.intellij.platform") version "2.5.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "2.2.1"
-    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "12.2.0"
 
     kotlin("jvm") version "2.1.10"
 }
