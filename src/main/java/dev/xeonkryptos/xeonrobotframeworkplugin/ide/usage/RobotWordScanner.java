6ff9c29580ecd554f8a8a0fcc594e3ed87e1ffb0
Dennis Föller <info@xeonkryptos.dev>
2025-07-14 09:42:22 +0200

    modified RobotWordsScanner

--- src/main/java/dev/xeonkryptos/xeonrobotframeworkplugin/ide/usage/RobotWordScanner.java
+++ src/main/java/dev/xeonkryptos/xeonrobotframeworkplugin/ide/usage/RobotWordScanner.java
@@ -1,11 +1,15 @@
 package dev.xeonkryptos.xeonrobotframeworkplugin.ide.usage;
 
 import com.intellij.lang.cacheBuilder.DefaultWordsScanner;
+import com.intellij.lang.cacheBuilder.VersionedWordsScanner;
+import com.intellij.lang.cacheBuilder.WordOccurrence;
 import com.intellij.psi.tree.TokenSet;
+import com.intellij.util.Processor;
 import dev.xeonkryptos.xeonrobotframeworkplugin.psi.RobotLexerAdapter;
 import dev.xeonkryptos.xeonrobotframeworkplugin.psi.RobotTypes;
+import org.jetbrains.annotations.NotNull;
 
-public class RobotWordScanner extends DefaultWordsScanner {
+public class RobotWordScanner extends VersionedWordsScanner {
 
     private static final TokenSet IDENTIFIERS = TokenSet.create(RobotTypes.USER_KEYWORD_STATEMENT,
                                                                 RobotTypes.KEYWORD_CALL,
@@ -16,9 +20,20 @@ public class RobotWordScanner extends DefaultWordsScanner {
     private static final TokenSet COMMENTS = TokenSet.create(RobotTypes.COMMENT);
     private static final TokenSet LITERALS = TokenSet.create(RobotTypes.POSITIONAL_ARGUMENT);
 
-    public RobotWordScanner() {
-        super(new RobotLexerAdapter(), IDENTIFIERS, COMMENTS, LITERALS);
+    private volatile DefaultWordsScanner myDelegate;
 
-        setMayHaveFileRefsInLiterals(true);
+    @Override
+    public void processWords(@NotNull CharSequence fileText, @NotNull Processor<? super WordOccurrence> processor) {
+        DefaultWordsScanner delegate = this.myDelegate;
+        if (delegate == null) {
+            myDelegate = delegate = new DefaultWordsScanner(new RobotLexerAdapter(), IDENTIFIERS, COMMENTS, LITERALS);
+            myDelegate.setMayHaveFileRefsInLiterals(true);
+        }
+        delegate.processWords(fileText, processor);
+    }
+
+    @Override
+    public int getVersion() {
+        return super.getVersion() + 1;
     }
 }
