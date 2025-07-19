30eb0e9fe84b97b6ef7588d0c898778ad670a5a7
Dennis Föller <2-Xeonkryptos@users.noreply.gitlab.xeonkryptos.dev>
2025-07-19 18:04:07 +0200

    removed unused imports

--- src/main/java/dev/xeonkryptos/xeonrobotframeworkplugin/ide/actionhandler/RobotKeywordCallEnterActionHandler.java
+++ src/main/java/dev/xeonkryptos/xeonrobotframeworkplugin/ide/actionhandler/RobotKeywordCallEnterActionHandler.java
@@ -1,12 +1,6 @@
 package dev.xeonkryptos.xeonrobotframeworkplugin.ide.actionhandler;
 
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.PsiTreeUtil;
 import dev.xeonkryptos.xeonrobotframeworkplugin.psi.element.RobotKeywordCall;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class RobotKeywordCallEnterActionHandler extends AbstractRobotSmartMultilineEnterActionHandler<RobotKeywordCall> {
 
