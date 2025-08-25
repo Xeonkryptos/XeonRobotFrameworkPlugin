5170a0922b129da8d776767ee8cd25ba1b1d8704
Dennis Föller <2-Xeonkryptos@users.noreply.gitlab.xeonkryptos.dev>
2025-08-25 09:43:09 +0200

    extended keyword-only marker checks in robot code

--- src/main/java/dev/xeonkryptos/xeonrobotframeworkplugin/ide/inspections/compilation/RobotLocalArgumentsSettingAnnotator.java
+++ src/main/java/dev/xeonkryptos/xeonrobotframeworkplugin/ide/inspections/compilation/RobotLocalArgumentsSettingAnnotator.java
@@ -4,12 +4,18 @@ import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.psi.PsiElement;
 import dev.xeonkryptos.xeonrobotframeworkplugin.RobotBundle;
 import dev.xeonkryptos.xeonrobotframeworkplugin.psi.element.RobotLocalArgumentsSetting;
+import dev.xeonkryptos.xeonrobotframeworkplugin.psi.element.RobotLocalArgumentsSettingArgument;
 import dev.xeonkryptos.xeonrobotframeworkplugin.psi.element.RobotVariableDefinition;
+import dev.xeonkryptos.xeonrobotframeworkplugin.psi.visitor.RecursiveRobotVisitor;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class RobotLocalArgumentsSettingAnnotator implements Annotator, DumbAware {
 
@@ -32,6 +38,40 @@ public class RobotLocalArgumentsSettingAnnotator implements Annotator, DumbAware
                           .withFix(new RemoveMoreThanOnceDefinedKeywordOnlyMarkersQuickFix())
                           .create();
                 }
+            }
+        }
+
+        if (keywordOnlyMarkerFound) {
+            RobotLocalArgumentsSettingArgumentVisitor visitor = new RobotLocalArgumentsSettingArgumentVisitor();
+            argumentsSetting.acceptChildren(visitor);
+            for (PsiElement invalidVariableDefinition : visitor.invalidVariableDefinitions) {
+                holder.newAnnotation(HighlightSeverity.ERROR,
+                                     RobotBundle.getMessage("annotation.user-keyword.settings.argument.variable-defined-after-keyword-only-marker"))
+                      .highlightType(ProblemHighlightType.GENERIC_ERROR)
+                      .range(invalidVariableDefinition)
+                      .create();
+            }
+        }
+    }
+
+    private static final class RobotLocalArgumentsSettingArgumentVisitor extends RecursiveRobotVisitor {
+
+        private final Set<PsiElement> invalidVariableDefinitions = new LinkedHashSet<>();
+
+        private boolean keywordOnlyMarkerFound = false;
+
+        @Override
+        public void visitLocalArgumentsSettingArgument(@NotNull RobotLocalArgumentsSettingArgument o) {
+            // Avoid visiting any children of this kind. We will find other variable definitions and those we don't have to take a look at
+        }
+
+        @Override
+        public void visitVariableDefinition(@NotNull RobotVariableDefinition o) {
+            ProgressManager.checkCanceled();
+            if (o.getName() == null) {
+                keywordOnlyMarkerFound = true;
+            } else if (keywordOnlyMarkerFound) {
+                invalidVariableDefinitions.add(o);
             }
         }
     }
