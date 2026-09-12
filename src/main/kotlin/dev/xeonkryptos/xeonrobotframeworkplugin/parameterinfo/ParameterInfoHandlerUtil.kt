f728c4829d5e59ee4477e0e23dc6221fc83211fb
Xeonkryptos <info@xeonkryptos.dev>
2026-09-12 12:34:42 +0200

    Revert "removed usages of ExecutionBundle"
    This reverts commit 3f1ffb2c0f4c1d4d21a3324838487935bf841a2b.

--- src/main/kotlin/dev/xeonkryptos/xeonrobotframeworkplugin/parameterinfo/ParameterInfoHandlerUtil.kt
+++ src/main/kotlin/dev/xeonkryptos/xeonrobotframeworkplugin/parameterinfo/ParameterInfoHandlerUtil.kt
@@ -84,7 +84,7 @@ object ParameterInfoHandlerUtil {
             val callableType = typeEvalContext.getType(callingFunction) as? PyCallableType? ?: return null
 
             hintsAndAnnotations = buildParameterListHint(parameters, indexToNamedParameter, parameterToHintIndex, hintFlags, typeEvalContext)
-            highlightParameters(mutableListOf(*arguments), callableType, parameters, parameterToHintIndex, hintFlags, currentParamIndex)
+            highlightParameters(mutableListOf(*arguments), callableType, parameters, indexToNamedParameter, parameterToHintIndex, hintFlags, currentParamIndex)
         } else {
             val parameterToHintIndex: MutableMap<DefinedParameter, Int> = mutableMapOf()
 
@@ -217,9 +217,17 @@ object ParameterInfoHandlerUtil {
     fun highlightParameters(arguments: MutableList<RobotArgument>,
                             callableType: PyCallableType,
                             parameters: MutableList<PyCallableParameter>,
+                            indexToNamedParameter: MutableMap<Int, PyCallableParameter>,
                             parameterToHintIndex: MutableMap<PyCallableParameter, Int>,
                             hintFlags: MutableMap<Int, EnumSet<ParameterInfoUIContextEx.Flag>>,
                             currentParamIndex: Int) { // gray out enough first parameters as implicit (self, cls, ...)
+        for (i in 0..<callableType.implicitOffset) {
+            if (indexToNamedParameter.containsKey(i)) {
+                val parameter = indexToNamedParameter[i]
+                hintFlags[parameterToHintIndex[parameter]]!!.add(ParameterInfoUIContextEx.Flag.DISABLE) // show but mark as absent
+            }
+        }
+
         collectHighlights(callableType, parameters, parameterToHintIndex, hintFlags, arguments, currentParamIndex)
     }
 
